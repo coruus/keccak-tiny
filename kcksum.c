602d9269dfa4a172a64e76fb5a2126a06e1f952e
David Leon Gil <coruus@gmail.com>
2014-05-19 14:45:13 -0400

    Added ninja-files. Removed lstat/fstat consistency check from kcksum.c; this only matters, in general, for setuid programs.

--- kcksum.c
+++ kcksum.c
@@ -11,12 +11,10 @@
 #include <unistd.h>
 
 #define ENO(LABEL, MSG) \
-  do {                 \
-    if (err != 0) {    \
+  _(if (err != 0) {    \
       perror(MSG);     \
       goto LABEL;      \
-    }                  \
-  } while (0)
+    })
 
 #define E(LABEL, MSG)                         \
   do {                                        \
@@ -26,6 +24,12 @@
       goto LABEL;                             \
     }                                         \
   } while (0)
+
+#ifdef VERBOSE
+#define verbose(...) printf(__VA_ARGS__);
+#else
+#define verbose(...)
+#endif
 
 int main(int argc, char** argv) {
   if (argc < 2) {
@@ -38,12 +42,6 @@ int main(int argc, char** argv) {
 
   fprintf(stderr, "hashing '%s'", argv[1]);
 
-  err = lstat(argv[1], &stat);
-  ENO(ret, "couldn't stat");
-
-  // Store the length of the file we statted.
-  size_t llength = (size_t)stat.st_size;
-
   int fd = open(argv[1], O_RDONLY | O_NONBLOCK | O_NOFOLLOW);
   err = !fd;
   E(ret, "couldn't open");
@@ -54,9 +52,6 @@ int main(int argc, char** argv) {
   size_t length = (size_t)stat.st_size;
   if (length == 0) {
     E(close_file, "length-0 file");
-  } else if (llength != length) {
-    E(close_file,
-      "fstat.st_len != lstat.st_len");
   }
 
   fprintf(stderr, ", length=%zu..\n", length);
@@ -67,15 +62,11 @@ int main(int argc, char** argv) {
   }
 
   uint8_t out[OBYTES] = {0};
-  fprintf(stderr, "..\n");
   err = SHAFN(out, OBYTES, in, length);
 
-  printf("%s('%s') = ", NAME, argv[1]);
-  for (size_t i = 0; i < OBYTES; i++) {
-    printf("%02x", out[i]);
-  }
-  printf("\n");
-  fprintf(stderr, "..done.\n");
+  verbose("%s('%s') = ", NAME, argv[1]);
+  FOR(i, 1, OBYTES, printf("%02x", out[i]));
+  verbose("\n");
   munmap(in, length);
 
   close_file:
---
+++ scripts/count.sh
@@ -0,0 +1,1 @@
+cloc f202.c f202.h kck.c kcksum.c u.h
