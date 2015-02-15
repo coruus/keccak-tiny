d1412ddf746ab81652c8bb5cdae6f05419bb7713
David Leon Gil <coruus@gmail.com>
2015-02-14 16:12:05 -0800

    Updated shell scripts.

--- dokats.sh
+++ dokats.sh
@@ -2,4 +2,4 @@
 ninja
 test -d build/answers && rm -rf build/answers
 mkdir build/answers &&
-parallel -j8 './build/shake256sum {} > build/answers/{/} 2>/dev/null' ::: kats/input/*
+parallel -j8 './build/shake256sum {} > build/answers/{/}' ::: kats/input/*
