a39886a925afeb0d78ad434f4d04ef2eb2defe7b
David Leon Gil <coruus@gmail.com>
2015-03-09 20:29:43 -0700

    Updated shake256.c to use Michael Hamburg's prettier round constants.

--- shake256.c
+++ shake256.c
@@ -26,30 +26,14 @@ static const uint8_t rho[24] = {1,  3,  6,  10, 15, 21, 28, 36, 45, 55, 2,  14,
                                 27, 41, 56, 8,  25, 43, 62, 18, 39, 61, 20, 44};
 static const uint8_t pi[24] = {10, 7,  11, 17, 18, 3, 5,  16, 8,  21, 24, 4,
                                15, 23, 19, 13, 12, 2, 20, 14, 22, 9,  6,  1};
-static const uint64_t RC[24] = {1ULL,
-                                0x8082ULL,
-                                0x800000000000808aULL,
-                                0x8000000080008000ULL,
-                                0x808bULL,
-                                0x80000001ULL,
-                                0x8000000080008081ULL,
-                                0x8000000000008009ULL,
-                                0x8aULL,
-                                0x88ULL,
-                                0x80008009ULL,
-                                0x8000000aULL,
-                                0x8000808bULL,
-                                0x800000000000008bULL,
-                                0x8000000000008089ULL,
-                                0x8000000000008003ULL,
-                                0x8000000000008002ULL,
-                                0x8000000000000080ULL,
-                                0x800aULL,
-                                0x800000008000000aULL,
-                                0x8000000080008081ULL,
-                                0x8000000000008080ULL,
-                                0x80000001ULL,
-                                0x8000000080008008ULL};
+#define RC_B(x,n) ((((x##ull)>>n)&1)<<((1<<n)-1))
+#define RC_X(x) (RC_B(x,0)|RC_B(x,1)|RC_B(x,2)|RC_B(x,3)|RC_B(x,4)|RC_B(x,5)|RC_B(x,6))
+static const uint64_t RC[24] = {
+    RC_X(0x01), RC_X(0x1a), RC_X(0x5e), RC_X(0x70), RC_X(0x1f), RC_X(0x21),
+    RC_X(0x79), RC_X(0x55), RC_X(0x0e), RC_X(0x0c), RC_X(0x35), RC_X(0x26),
+    RC_X(0x3f), RC_X(0x4f), RC_X(0x5d), RC_X(0x53), RC_X(0x52), RC_X(0x48),
+    RC_X(0x16), RC_X(0x66), RC_X(0x79), RC_X(0x58), RC_X(0x21), RC_X(0x74)
+};
 
 /** Keccak-f[1600] **/
 
